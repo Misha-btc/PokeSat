5ed24659229d6438629e80927474f52e817965d1
misha sycheff <tuskar.israel@gmail.com>
2023-10-31 08:22:04 +0200

    blogicum4.2

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -9,55 +9,55 @@ urlpatterns = [
         '',
         views.PostListView.as_view(),
         name='index'
-        ),
+    ),
     path(
         'posts/<int:post_id>/',
         views.PostDetailView.as_view(),
         name='post_detail'
-        ),
+    ),
     path(
         'category/<slug:category_slug>/',
         views.CategoryDetailView.as_view(),
         name='category_posts'
-        ),
+    ),
     path(
         'posts/create/',
         views.PostCreateView.as_view(),
         name='create_post'
-        ),
+    ),
     path(
         'profile/<slug:username>/',
         views.ProfileDetailView.as_view(),
         name='profile'
-        ),
+    ),
     path(
         'profile/<slug:username>/edit/',
         views.ProfileUpdateView.as_view(),
         name='edit_profile'
-        ),
+    ),
     path(
         'posts/<int:post_id>/edit/',
         views.PostUpdateView.as_view(),
         name='edit_post'
-        ),
+    ),
     path(
         'posts/<int:post_id>/delete/',
         views.PostDeleteView.as_view(),
         name='delete_post'
-        ),
+    ),
     path(
         'posts/<int:post_id>/comment/',
         views.AddComment.as_view(),
         name='add_comment'
-        ),
+    ),
     path(
         'posts/<int:post_id>/edit_comment/<int:comment_id>/',
         views.CommentUpdateView.as_view(),
         name='edit_comment'
-        ),
+    ),
     path(
         'posts/<int:post_id>/delete_comment/<int:comment_id>/',
         views.CommentDeleteView.as_view(),
         name='delete_comment'
-        ),
+    ),
 ]
