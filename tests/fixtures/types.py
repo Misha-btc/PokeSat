1c276ef9326ba1a5d9337853903d46054ba428eb
misha sycheff <tuskar.israel@gmail.com>
2023-11-25 23:56:30 +0200

    Deploy

--- tests/fixtures/types.py
+++
@@ -1,7 +0,0 @@
-from typing import TypeVar, Union
-
-from adapters.post import PostModelAdapter
-from adapters.user import UserModelAdapter
-
-CommentModelAdapterT = TypeVar("CommentModelAdapterT", bound=type)
-ModelAdapterT = Union[CommentModelAdapterT, PostModelAdapter, UserModelAdapter]
