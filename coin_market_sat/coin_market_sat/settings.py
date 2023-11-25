f06829b494bf70da703b9a2e21cb8c218caf9e14
Maxim <maxime.bocellia@gmail.com>
2023-11-26 00:47:41 +0200

    test2

--- coin_market_sat/coin_market_sat/settings.py
+++ coin_market_sat/coin_market_sat/settings.py
@@ -11,22 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q94(kut8pew+2&npg9$!$1=761oj16c3*w^+++hw=v%(6&11u2'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q94(kut8pew+2&npg9$!$1=761oj16c3*w^+++hw=v%(6&11u2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['pokesat.herokuapp.com', '127.0.0.1']  # Замените 'your-app-name.herokuapp.com' на название своего приложения на Heroku
 
 # Application definition
 
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,20 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Укажите каталог разработки для статических файлов
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_dev'),  # Изменил BASE_DIR / 'static_dev' на os.path.join(BASE_DIR, 'static_dev')
+    os.path.join(BASE_DIR, 'static_dev'),
 ]
-
-# Укажите каталог для сбора статических файлов
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Изменил '/static' на os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
