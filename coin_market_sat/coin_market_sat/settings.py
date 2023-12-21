c2be12579206db71ae4d288cb1a00613940392b0
Misha Sychev <91029010+Misha-btc@users.noreply.github.com>
2023-12-21 03:31:55 +0200

    Update settings.py

--- coin_market_sat/coin_market_sat/settings.py
+++ coin_market_sat/coin_market_sat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q94(kut8pew+2&npg9$!$1=761oj16c3*w^+++hw=v%(6&11u2')
+#SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
